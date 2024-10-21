e438cf9fdf376a1e7a786ab1b41f8da27210e2d2
udvardiendre <udvardiendre@gmail.com>
2024-10-21 20:54:35 +0200

    some works on the services

--- components/ui/service-card.tsx
+++ components/ui/service-card.tsx
@@ -10,10 +10,10 @@ const ServiceCard = ({
   title: string;
 }) => {
   return (
-    <div className="">
-      <Image src={image} alt={id} />
-      <div>
-        <h3>{title}</h3>
+    <div className=" bg-white shadow-lg rounded-[30px] w-[322px] h-[312px]">
+      <Image className="w-full h-2/3 rounded-t-[30px]" src={image} alt={id} />
+      <div className="h-1/3 flex items-start justify-center text-center mt-2 p-2">
+        <h3 className="text-[1.25rem]/[1.5rem] font-semibold uppercase mb-12">{title}</h3>
       </div>
     </div>
   );
